8da429b81abbb0f3f7f3861beaa2a4c64d7a1000
junsong-cang <cangjunsong@outlook.com>
2026-07-16 11:28:12 +0200

    Cheched again that I can get Tr by integrating over SFRD

--- src/py21cmfirstclass/src/SpinTemperatureBox.c
+++ src/py21cmfirstclass/src/SpinTemperatureBox.c
@@ -62,7 +62,6 @@ int ComputeTsBox(float redshift, float prev_redshift, struct UserParams *user_pa
         }
         /*
         TODO: Junsong
-        Finally check that i can get TR by integrating over SFRD?
         Double check Radio Heating
         For a template run, print SFRD & EoR, use it to compute dTff/dz externally and check with p21f
         Add additional soft photon variables to pragma thing!!!! -> dT_Radio_FF
